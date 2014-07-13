02032d382eedfe400ab4363b7bd6970e51b2ef66
Marcelo Barros de Almeida <marcelobarrosalmeida@gmail.com>
2014-07-13 11:45:40 -0300

    SW-151 Returning to the multithreaded version (cancel is possible)

--- software/openApps/udpStorm/udp_storm.py
+++ software/openApps/udpStorm/udp_storm.py
@@ -3,7 +3,8 @@ import sys
 from Tkinter import *
 import tkMessageBox
 import re
-import time
+from Queue import Queue
+import threading
 import struct
 
 p = os.path.dirname(sys.argv[0])
@@ -12,13 +13,70 @@ p = os.path.abspath(p)
 sys.path.insert(0,p)
 
 from coap import coap
-from coap import coapDefines as d
 
+class SendCoap(threading.Thread):
+    def __init__(self,addr,method,period,win,queue):
+        threading.Thread.__init__(self)
+        self.addr = addr
+        self.method = method
+        self.period = period
+        self.win = win
+        self.queue = queue
+        self.running = False
+        self.coap = coap.coap()
+        self.timer_cbk = None
+        self.tmr = 0
+
+    def wait_answer(self):
+        self.tmr = self.tmr + 1
+        if self.running:
+            self.timer_cbk = self.win.after(1000,self.wait_answer)
+            self.update_status("STATUS","Waiting answer ({0}) ...".format(self.tmr))
+
+    def exec_request(self):
+        if self.method == 'GET':
+            uri = 'coap://[{0}]/strm/period'.format(self.addr)
+            r = self.coap.GET(uri)
+            self.per = struct.unpack('>H',''.join([ chr(c) for c in r ]))[0]
+            self.update_status("PERIOD",self.per)
+        else:
+            uri = 'coap://[{0}]/strm/period'.format(self.addr)
+            per = [ ord(c) for c in struct.pack('>H',self.period) ]
+            r = self.coap.PUT(uri,payload=per)
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.tmr = 0
+            self.update_status("STATUS","Sending coap")
+            self.timer_cbk = self.win.after(1000,self.wait_answer)
+            try:
+                self.exec_request()
+            except Exception, e:
+                self.update_status("STATUS","Failed {0}".format(repr(e)))
+            else:
+                self.update_status("STATUS","Success !")
+            self.win.after_cancel(self.timer_cbk)
+            self.running = False
+
+    def quit(self):
+        self.coap.close()
+        if self.timer_cbk:
+            self.win.after_cancel(self.timer_cbk)
+        self.running = False
+
+    def is_running(self):
+        return self.running
+
+    def update_status(self,t,m):
+        self.queue.put((t,m))
+        try:
+            self.win.event_generate('<<UpdateStatus>>', when='tail')
+        except:
+            pass
 
 class UDPStormGUI(object):
-
     def __init__(self, master):
-
         self.master = master
         self.ipv6_addr = StringVar()
         self.ipv6_addr.set('bbbb::')
@@ -26,9 +84,24 @@ class UDPStormGUI(object):
         self.period.set(0)
         self.status = StringVar()
         self.status.set('')
-        self.coap = coap.coap()
+
         self.create_gui()
+
+        self.task = None
+        self.queue = Queue()
+        self.master.bind('<<UpdateStatus>>', self.update_status)
+
         self.master.mainloop()
+
+    def update_status(self,event):
+        t,m = self.queue.get()
+        if t == "PERIOD":
+            self.period.set(m)
+        elif t == "STATUS":
+            self.status.set(m)
+        else:
+            self.status.set(repr(m))
+        self.master.update_idletasks()
 
     def create_gui(self):
         self.master.wm_title("UDP Storm")
@@ -39,17 +112,17 @@ class UDPStormGUI(object):
 
         f = Frame(self.master,height=2,bd=1,relief=SUNKEN,padx=5,pady=5)
         f.pack(side=TOP,fill=X)
-        
+
         f = Frame(self.master,padx=5,pady=5)
         Label(f,text="coap://[").pack(side=LEFT,expand=NO)
         Entry(f,textvariable=self.ipv6_addr,width=40).pack(side=LEFT,expand=YES,fill=X)
         Label(f,text="]/strm/period=").pack(side=LEFT,expand=NO)
         Entry(f,textvariable=self.period,width=10).pack(side=LEFT,expand=NO)
         f.pack(side=TOP,expand=YES,fill=X)
-        
+
         f = Frame(self.master,height=2,bd=1,relief=SUNKEN,padx=5,pady=5)
         f.pack(side=TOP,fill=X)
-        
+
         f = Frame(self.master,padx=5,pady=5)
         Button(f,text="GET",width=10,command=self.get_cmd,default=ACTIVE).pack(side=RIGHT,expand=NO)
         Button(f,text="PUT",width=10,command=self.put_cmd,default=ACTIVE).pack(side=RIGHT,expand=NO)
@@ -57,22 +130,22 @@ class UDPStormGUI(object):
         f.pack(side=TOP,expand=YES,fill=X)
 
         self.master.protocol("WM_DELETE_WINDOW", self.close)
-        
+
     def validate(self,check_per=True):
         if check_per:
-        try:
-            per = self.period.get()
-        except:
-            tkMessageBox.showwarning("Period","Invalid number for period")
-            return False
-        
-        if (per < 0) or (per > (2**16-1)):
-            tkMessageBox.showwarning("Period","Invalid range for period [0-65535]")
-            return False
-        
+            try:
+                per = self.period.get()
+            except:
+                tkMessageBox.showwarning("Period","Invalid number for period")
+                return False
+
+            if (per < 0) or (per > (2**16-1)):
+                tkMessageBox.showwarning("Period","Invalid range for period [0-65535]")
+                return False
+
         ipv6 = self.ipv6_addr.get().strip()
         self.ipv6_addr.set(ipv6)
-        
+
         # http://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
         pattern = r"\b(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|" + \
             r"([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|" + \
@@ -82,54 +155,46 @@ class UDPStormGUI(object):
             r"::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|" + \
             r"(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|" + \
             r"1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))\b"
-        
+
         if re.match(pattern, ipv6):
             return True
         else:
             tkMessageBox.showwarning("IPv6","Invalid IPv6 address")
             return False
-            
+
         return True
 
+    def kill_task(self):
+        if self.task:
+            if self.task.is_running():
+                self.status.set('Cancelling')
+                self.master.update_idletasks()
+            self.task.quit()
 
     def get_cmd(self, event=None):
         if self.validate(False):
+            self.kill_task()
             self.status.set('')
             self.master.update_idletasks()
             ipv6 = self.ipv6_addr.get().strip()
             per = self.period.get()
-            uri = 'coap://[{0}]/strm/period'.format(ipv6)
-            self.status.set(uri)
-            try:
-                r = self.coap.GET(uri)
-                per = struct.unpack('>H',''.join([ chr(c) for c in r ]))[0]
-            except Exception, e:
-                self.status.set('Failed: {0}'.format(repr(e)))
-            else:
-                self.period.set(per)
-                self.status.set('GET OK!')
+            self.task = SendCoap(ipv6,'GET',per,self.master,self.queue)
+            self.task.start()
 
     def put_cmd(self, event=None):
         if self.validate():
+            self.kill_task()
             self.status.set('')
             self.master.update_idletasks()
             ipv6 = self.ipv6_addr.get().strip()
-            uri = 'coap://[{0}]/strm/period'.format(ipv6)
             per = self.period.get()
-            per = [ ord(c) for c in struct.pack('>H',per) ]
-            self.status.set(uri)
-            try:
-                r = self.coap.PUT(uri,payload=per)
-            except Exception, e:
-                self.status.set('Failed: {0}'.format(repr(e)))
-            else:
-                self.status.set('PUT OK!')
-        
+            self.task = SendCoap(ipv6,'PUT',per,self.master,self.queue)
+            self.task.start()
+
     def close(self):
-        if self.coap:
-            self.coap.close()
+        self.kill_task()
         self.master.destroy()
-        
+
 UDPStormGUI(Tk())
 
 
